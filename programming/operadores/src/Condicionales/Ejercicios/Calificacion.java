5e7058ecb24dbb0351d0ee781dfaafd805fc6b52
Kevin Hernandez <kevinhdez592@gmail.com>
2026-05-05 16:47:45 -0600

    reto ejercicios de condicionales

---
+++ programming/operadores/src/Condicionales/Ejercicios/Calificacion.java
@@ -0,0 +1,16 @@
+package Condicionales.Ejercicios;
+
+public class Calificacion {
+
+    /**
+     * Declara tres variables para tres notas de un examen (ej. nota1, nota2, nota3).
+     * Calcula el promedio.
+     * Si el promedio es mayor o igual a 6.0, imprime "Aprobado", si no, "Reprobado".
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ programming/operadores/src/Condicionales/Ejercicios/SensorTemperatura.java
@@ -0,0 +1,23 @@
+package Condicionales.Ejercicios;
+
+public class SensorTemperatura {
+    
+    /**
+     * Simula un sensor de una máquina.
+     * 
+     * Si la temperatura es menor a 10, imprime "Alerta: Temperatura muy baja".
+     * 
+     * Si la temperatura esta entre 10 y 30, imprime "Temperatura normal".
+     * 
+     * Si la temperatura es mayor a 30, imprime "Alerta: Sobrecalentamiento"
+     * 
+     */
+
+
+    public static void main(String[] args) {
+
+        
+    }
+     
+    
+}
