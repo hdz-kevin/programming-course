821f43dcbc742697372f096c1d7092a05fece92e
Kevin Hernandez <kevinhdez592@gmail.com>
2026-05-06 22:35:12 -0600

    ejercicio: sensor de temperatura

--- programming/operadores/src/Condicionales/Ejercicios/SensorTemperatura.java
+++ programming/operadores/src/Condicionales/Ejercicios/SensorTemperatura.java
@@ -10,14 +10,21 @@ public class SensorTemperatura {
      * Si la temperatura esta entre 10 y 30, imprime "Temperatura normal".
      * 
      * Si la temperatura es mayor a 30, imprime "Alerta: Sobrecalentamiento"
-     * 
      */
 
 
     public static void main(String[] args) {
 
+        int temperatura = 50;
+        
+        if (temperatura < 10) {
+            System.out.println("Alerta: Temperatura muy baja");
+        } else if (temperatura <= 30) {
+            System.out.println("Temperatura normal");
+        } else {
+            System.out.println("Alerta: Sobrecalentamiento");
+        }
         
     }
-     
-    
+
 }
